ec6424b02fdb8ebdcab1781d7b9e41a7213bc0f1
M.D. van Es <es@mdworld.nl>
2020-10-06 15:33:28 +0200

    Test origin

--- tinycors.go
+++ tinycors.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"flag"
-	// "fmt"
+	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
+  "net/http"
+  "reflect"
+  "errors"
 )
 
 const (
@@ -15,13 +17,36 @@ const (
 func ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
+type arrayOrigins []string
+
+func (i *arrayOrigins) String() string {
+  return "my string representation"
+}
+
+func (i *arrayOrigins) Set(value string) error {
+  *i = append(*i, value)
+  return nil
+}
+
+var allowedOrigins arrayOrigins
+
 func main() {
-	var port = flag.String("port", defaultPort, "the port of the server")
-	flag.Parse()
+  var port = flag.String("port", defaultPort, "the port of the server")
+  flag.Var(&allowedOrigins, "origins", "allowed origins, e.g. -origins http://localhost:3000 -origins http://localhost:8080")
+  flag.Parse()
+  fmt.Printf("origins: %+q\n", allowedOrigins)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     enableCors(&w)
+
+    origin := r.Header.Get("origin")
     
+    err := checkOrigin(w, origin)
+    if err != nil {
+			respondWithErr(w, err.Error())
+      return
+    }
+
 		queryUrl := r.URL.Query().Get("url")
 		if queryUrl == "" {
 			respondWithErr(w, "empty url")
@@ -59,9 +84,36 @@ func main() {
 	}
 }
 
+func itemExists(arrayType interface{}, item interface{}) bool {
+	arr := reflect.ValueOf(arrayType)
+
+  // TODO
+	// if arr.Kind() != reflect.Array {
+	// 	panic("Invalid data-type")
+	// }
+
+	for i := 0; i < arr.Len(); i++ {
+		if arr.Index(i).Interface() == item {
+			return true
+		}
+	}
+
+	return false
+}
+
 func enableCors(w *http.ResponseWriter) {
   (*w).Header().Set("Access-Control-Allow-Origin", "*")
   (*w).Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+}
+
+// If supplied originIncludeList is set use that, otherwise allow everything
+func checkOrigin(w http.ResponseWriter, origin string) error {
+  if len(allowedOrigins) > 0 && !itemExists(allowedOrigins, origin) {
+    log.Println("origin", origin, "is not in includelist")
+    w.WriteHeader(http.StatusForbidden)
+    return errors.New("origin is not in includelist")
+  }
+  return nil
 }
 
 func respondWithErr(w http.ResponseWriter, err string) {
